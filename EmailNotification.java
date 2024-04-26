e51800e988335af9a2cc209a71aa1a53564489a5
Jhoanna May Lacorte <152822639+JhoannaMay@users.noreply.github.com>
2024-04-26 14:38:55 +0000

    OOP_LAB ASSIGNMENT 6

---
+++ EmailNotification.java
@@ -0,0 +1,7 @@
+public class EmailNotification implements Notification {
+
+    @Override
+    public void sendEmailNotification(String email) {
+        System.out.println("Email notification sent to: " + email);
+    }
+}
